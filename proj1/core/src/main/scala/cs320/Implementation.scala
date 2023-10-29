a9f5e7a6f934445d3335fb08ec107edb276a85db
david3684 <chan3684@gmail.com>
2023-10-29 19:39:20 +0900

    dsad

--- proj1/core/src/main/scala/cs320/Implementation.scala
+++ proj1/core/src/main/scala/cs320/Implementation.scala
@@ -37,16 +37,17 @@ object Implementation extends Template {
       (leftVal, rightVal) match {
         case (IntV(x), IntV(y)) => 
           if(y!=0) IntV(x%y)
-          else error(s"Can't Divide by Zero")
+          else error(s"Can't divide by zero")
         case _ => error(s"Both operands should be integer")
       }
+
     case Div(left, right) => 
       val leftVal = helper(left, env)
       val rightVal = helper(right, env)
       (leftVal, rightVal) match {
         case (IntV(x), IntV(y)) => 
           if(y!=0) IntV(x/y)
-          else error(s"Can't Divide by Zero")
+          else error(s"Can't divide by zero")
         case _ => error(s"Both operands should be integer")
       }
     
@@ -55,17 +56,20 @@ object Implementation extends Template {
       case BooleanV(false) => helper(falseBranch, env)
       case _ => error(s"Condition should return a boolean value")
     }
+
     case TupleE(exprL) => 
       val valL = exprL.map(expr => helper(expr, env))
       TupleV(valL)
+
     case Proj(l, index) => 
       val valL = helper(l, env)
       valL match{
         case TupleV(values) => 
           if(index>=1 && index <= values.length) values(index-1)
-          else error(s"Index Out of Bound of length ${values.length}")
-        case _ => error(s"Projection can be applied for tuples")
+          else error(s"Index out of bound")
+        case _ => error(s"Projection can be applied only for tuples")
       }
+
     case ConsE(head, tail) =>   
       val headV = helper(head, env)
       val tailV = helper(tail, env)
@@ -74,6 +78,7 @@ object Implementation extends Template {
         case (_, ConsV(_,_)) => ConsV(headV, tailV)
         case _ => error(s"Second Argument should be a list")
       }
+
     case Empty(l) =>
       val listVal = helper(l, env)
       listVal match{
@@ -81,45 +86,46 @@ object Implementation extends Template {
         case ConsV(_,_) => BooleanV(false)
         case _ => error(s"Given expr is not a list")
       }
+
     case Head(l) => 
       val listVal = helper(l, env)
       listVal match{
         case ConsV(head,_) => head
         case _ => error(s"Given expr is not a list")
       }
+
     case Tail(l) => 
       val listVal = helper(l, env)
       listVal match{
         case ConsV(_,tail) => tail
         case _ => error(s"Given expr is not a list")
       }
+
     case Val(n, e, b) => helper(b, env+(n->helper(e,env)))
+
     case Fun(params, b) => CloV(params, b, env)
-    case RecFuns(f, b) => 
-      // Create a new environment with dummy bindings for the function names
-      val dummyEnv = f.foldLeft(env) {
-        case(currentEnv, FunDef(fname,params, _)) => 
-          currentEnv + (fname -> CloV(params, IntE(0), currentEnv))
-      }
-      // Create actual closures with the dummy environment
-      val funClosures = f.map {
-        case FunDef(fname, params, fbody) => 
-          fname -> CloV(params, fbody, dummyEnv)
+
+
+    case RecFuns(f: List[FunDef], b: Expr) => 
+      
+      val Closures = f.map{
+        case FunDef(fname, params, fbody) =>
+          fname -> CloV(params, fbody, Map())  
       }.toMap
-        // Update the dummy environment with the actual closures
-      val newEnv = dummyEnv ++ funClosures
-      // Evaluate the body expression in the updated environment
+      val newEnv = env ++ Closures
+      
+      Closures.values.foreach(clo => clo.env = newEnv)  
+      
       helper(b, newEnv)
+
+
     case App(f, args) => helper(f, env) match{
-      case CloV(params, b, fenv) => {
+      case CloV(params, ec, fenv) => {
         val avals = args.map(helper(_,env))
         if(args.length != params.length){
-          error(s"Function application error: Expected ${params.length} parameters, but got ${args.length}.")
-          error(s"Expected parameters: $params")
-          error(s"Received arguments: $args")
-          error(s"Wrong Arity") 
-        } 
-        helper(b, fenv ++ (params zip avals))
+          error(s"Wrong arity") 
+        }
+        helper(ec, fenv ++ (params zip avals))
       }
       case _ => error(s"Call a correct function")
     }
@@ -151,38 +157,7 @@ object Implementation extends Template {
   } 
 }
 
-/*RecFuns(
-  List(
-    FunDef(
-      f,
-      List(x),
-      If(
-        Eq(
-          Id(x),
-          IntE(0)
-          ),
-        IntE(0),
-        Add(Id(x),
-        App(
-          Id(f),
-          List(
-            Add(
-              Id(x),
-              Mul(
-                IntE(1),
-                IntE(-1)
-                )
-              )
-            )
-          )
-        )
-      )
-    )
-  ),
-  App(
-    Id(f),
-    List(
-      IntE(3)
-      )
-    )
-  )*/
+
+
+
+
